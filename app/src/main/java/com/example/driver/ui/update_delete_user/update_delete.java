19b48f024c8984e6f453112bce03e01fcc91ae41
DELL <yousranabil23@gmail.com>
2020-09-20 20:58:58 +0300

    by Yousra

---
+++ app/src/main/java/com/example/driver/ui/update_delete_user/update_delete.java
@@ -0,0 +1,26 @@
+package com.example.driver.ui.update_delete_user;
+
+public class update_delete {
+
+    int txtinfo;
+    String txtInfoUser;
+
+
+    public int getTxtinfo() {
+        return txtinfo;
+    }
+
+    public void setTxtinfo(int txtinfo) {
+        this.txtinfo = txtinfo;
+    }
+
+    public String getTxtInfoUser() {
+        return txtInfoUser;
+    }
+
+    public void setTxtInfoUser(String txtInfoUser) {
+        this.txtInfoUser = txtInfoUser;
+    }
+
+
+}
---
+++ app/src/main/java/com/example/driver/ui/update_delete_user/update_delete_Adapter.java
@@ -0,0 +1,65 @@
+package com.example.driver.ui.update_delete_user;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.driver.R;
+import com.example.driver.VioAdapter;
+import com.example.driver.ui.update_delete_user.update_delete_Adapter;
+import com.example.driver.ui.police.VioType;
+import com.example.driver.ui.update_delete_user.update_delete_Adapter;
+import com.example.driver.ui.update_delete_user.update_delete;
+
+import java.util.List;
+
+class update_delete_Adapter extends RecyclerView.Adapter<update_delete_Adapter.UdateDelete> {
+
+    List<update_delete> UdateDelete;
+    Context context;
+
+    public update_delete_Adapter(List<update_delete> UdateDelete, Context context) {
+        this.UdateDelete = UdateDelete;
+        this.context = context;
+    }
+    @NonNull
+    @Override
+    public UdateDelete onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        UdateDelete updatedelete;
+
+        View v = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.info_user, parent, false);
+        updatedelete = new update_delete_Adapter.UdateDelete(v);
+
+        return updatedelete;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull UdateDelete holder, int position) {
+        update_delete update_delete = UdateDelete.get(position);
+    }
+
+    @Override
+    public int getItemCount() {
+        if (UdateDelete != null)
+            return UdateDelete.size();
+        else
+            return 0;
+    }
+
+    public class UdateDelete extends RecyclerView.ViewHolder {
+
+        TextView textView;
+
+        public UdateDelete(@NonNull View itemView) {
+            super(itemView);
+            textView = itemView.findViewById(R.id.item_info_user);
+        }
+    }
+}
