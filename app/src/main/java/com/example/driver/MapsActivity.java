a9cb474f81f3e069229bf631b9dafa1cf8daa27e
mohammed <meddce1997@gmail.com>
2020-10-11 02:02:49 +0300

    by mohammed 10-10-2020

--- app/src/main/java/com/example/driver/MapsActivity.java
+++ app/src/main/java/com/example/driver/MapsActivity.java
@@ -14,17 +14,28 @@ import com.google.android.gms.maps.model.MarkerOptions;
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    double latLocation, longLocation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+
+//how to share data between 2 fragments
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
     }
 
+
+    public void setLatLocation(double latLocation) {
+        this.latLocation = latLocation;
+    }
+
+    public void setLongLocation(double longLocation) {
+        this.longLocation = longLocation;
+    }
 
     /**
      * Manipulates the map once available.
@@ -35,12 +46,14 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
      */
+
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
+        LatLng sydney = new LatLng(latLocation, longLocation);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
--- app/src/main/java/com/example/driver/VioAdapter.java
+++ app/src/main/java/com/example/driver/VioAdapter.java
@@ -12,6 +12,7 @@ import android.widget.TextView;
 import java.util.List;
 
 import androidx.annotation.NonNull;
+import androidx.fragment.app.FragmentActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
 public class VioAdapter extends RecyclerView.Adapter {
@@ -41,23 +42,33 @@ public class VioAdapter extends RecyclerView.Adapter {
     public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
 
         final VioClass vioClass = vioClassList.get(position);
-        ((ViewHolder) holder).txtName.setText("Police name: "+vioClass.getName());
-        ((ViewHolder) holder).txtCarNumber.setText("car Num: "+vioClass.getCarNumber());
-        ((ViewHolder) holder).licence.setText("car Num: "+vioClass.getLicence());
-       ((ViewHolder) holder).txtType.setText("Vio Type: "+vioClass.getType());
-        ((ViewHolder) holder).txtDate.setText("Vio Date: " +vioClass.getDate());
+        ((ViewHolder) holder).txtName.setText("Police name: " + vioClass.getName());
+        ((ViewHolder) holder).txtCarNumber.setText("car Num: " + vioClass.getCarNumber());
+        ((ViewHolder) holder).licence.setText("car Num: " + vioClass.getLicence());
+        ((ViewHolder) holder).txtType.setText("Vio Type: " + vioClass.getType());
+        ((ViewHolder) holder).txtDate.setText("Vio Date: " + vioClass.getDate());
 
 
         ((ViewHolder) holder).goLocation.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-                String query = "?q=" + vioClass.getLatLocation() + "," + vioClass.getLongLocation() + "(" + vioClass.getName() +" car number: "+vioClass.getCarNumber()+ ")" + "&z=18";
-                String encodedQuery = Uri.encode(query);
-                Uri gmmIntentUri2 = Uri.parse("geo:" + vioClass.getLatLocation() + "," + vioClass.getLongLocation() + query);
-                Intent mapIntent2 = new Intent(Intent.ACTION_VIEW, gmmIntentUri2);
-                mapIntent2.setPackage("com.google.android.apps.maps");
-                context.startActivity(mapIntent2);
+//                String query = "?q=" + vioClass.getLatLocation() + "," + vioClass.getLongLocation() + "(" + vioClass.getName() + " car number: " + vioClass.getCarNumber() + ")" + "&z=18";
+//                String encodedQuery = Uri.encode(query);
+//
+//                Uri gmmIntentUri2 = Uri.parse("geo:" + vioClass.getLatLocation() + "," +
+//                        vioClass.getLongLocation() + query);
+//                Intent mapIntent2 = new Intent(Intent.ACTION_VIEW,
+//                        gmmIntentUri2);
+//                mapIntent2.setPackage("com.google.android.apps.maps");
+//                context.startActivity(mapIntent2);
+
+                MapsActivity mapsActivity = new MapsActivity();
+                mapsActivity.setLatLocation(vioClass.latLocation);
+                mapsActivity.setLongLocation(vioClass.longLocation);
+                //
+
+
 
             }
         });
@@ -74,7 +85,7 @@ public class VioAdapter extends RecyclerView.Adapter {
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
 
-        public TextView txtName,licence ;
+        public TextView txtName, licence;
         public TextView txtCarNumber;
         public TextView txtType;
         public TextView location;
@@ -86,12 +97,10 @@ public class VioAdapter extends RecyclerView.Adapter {
             super(itemView);
             txtName = itemView.findViewById(R.id.textView_name);
             txtCarNumber = itemView.findViewById(R.id.textView_number_car);
-           cardDate = itemView.findViewById(R.id.txt_card_date);
+            cardDate = itemView.findViewById(R.id.txt_card_date);
             txtType = itemView.findViewById(R.id.textView_type);
             txtDate = itemView.findViewById(R.id.text_driver_date);
-           licence = itemView.findViewById(R.id.txt_licence);
-
-
+            licence = itemView.findViewById(R.id.txt_licence);
 
 
         }
